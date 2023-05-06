2d9bfb5d3d259eb665411c9a96899554c5f9c65b
logan <logan.stundal@gmail.com>
2023-05-06 19:14:26 -0400

    initial commit

---
+++ scripts/kde-setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script sets up kde environment preferences using assets stored in /files/kde
+
+# -------------------------------------- #
+# Wallpaper
+# -------------------------------------- #
+ plasma-apply-wallpaperimage $(realpath ../)/files/kde/wall-nordic.jpg
+# -------------------------------------- #
+
+# -------------------------------------- #
+# Theme
+# -------------------------------------- #
+theme_dir="$HOME/.local/share/plasma/look-and-feel/"
+if [ ! -d  "$theme_dir/Nordic-darker" ]
+then
+   cp $(realpath ../)/files/kde/Nordic-darker $theme_dir
+fi
+
+plasma-apply-desktoptheme Nordic-darker
+# -------------------------------------- #
